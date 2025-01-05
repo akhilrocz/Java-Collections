868062c1ebbfe54cc7fe9621bfb8d18c05acf677
Akhil roczz <b.akhil667@gmail.com>
2025-01-05 23:04:49 +0530

    updated

--- HashMapDemo.java
+++ HashMapDemo.java
@@ -11,11 +11,17 @@ public static void main(String[] args) {
 //    System.out.println("Entry set "+hashMap.entrySet());  //entrySet() used for pair view of map
 
     hashMap.put(1,"Akhil");
+    System.out.println(hashMap.get(1));  //value which is not modified
+    hashMap.put(1,"Rajesh");
+    System.out.println(hashMap.get(1));  //printed modified value
     hashMap.put(2,"Nikhil");
     hashMap.put(3,"Raju");
 
-    System.out.println("map elements "+hashMap);
-    System.out.println("Entry set view: "+hashMap.entrySet());
+
+
+
+    System.out.println("map elements "+hashMap);  //{1=Akhil, 2=Nikhil 3=Raju}
+    System.out.println("Entry set view: "+hashMap.entrySet());  //output [1=Akhil, 2=Nikhil 3=Raju]
 
 
 
